189e4a1d0d35c8225f8c3960522b4517d09ff81e
Olatunde Sodiq <olatundesodiq@gmail.com>
2020-06-06 00:45:18 +0100

    add github action test

--- banka/api/tests/test_githubAction.py
+++ banka/api/tests/test_githubAction.py
@@ -4,4 +4,6 @@ from rest_framework.test import APITestCase
 class TestGithubAction(APITestCase):
     def test_github_actions(self):
         self.assertEqual(2 + 2, 4)
-        
+
+    def test_github_action(self):
+        self.assertFalse(not True)
