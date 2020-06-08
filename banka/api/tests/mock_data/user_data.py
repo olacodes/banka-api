405ef2e75ccfa324b1730958d2eea256229c8abf
Olatunde Sodiq <olatundesodiq@gmail.com>
2020-06-08 18:03:01 +0100

    test user signup api endpoint :star:

---
+++ banka/api/tests/mock_data/user_data.py
@@ -0,0 +1,15 @@
+
+
+class UserData:
+
+    @staticmethod
+    def user_signup_data():
+        return {
+           "firstname": "test",
+            "lastname": "testing",
+            "password": "testsecret",
+            "email": "test@gmail.com",
+            "user_type": "staff",
+            "is_admin": "true"
+        }
+        
